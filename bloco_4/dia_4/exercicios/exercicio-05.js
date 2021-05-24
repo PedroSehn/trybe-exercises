20e9829f49f900f62f987b9a9b73e958005b5f5c
Pedro Sehn <pedrorsehn@hotmail.com>
2021-05-24 18:45:41 -0300

    add alguns exercicios

---
+++ bloco_4/dia_4/exercicios/exercicio-05.js
@@ -0,0 +1,19 @@
+let info = {
+	personagem: 'Margarida',
+	origem: 'Pato Donald',
+	nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+	recorrente: 'sim'
+  };
+
+
+  let tioPatinhas = {
+	personagem: 'Tio Patinhas',
+	origem: 'Christmas on Bear Mountain, Dell Four Color Comics #178',
+	nota: 'O último MacPatinhas',
+	recorrente: 'sim'
+  };
+
+
+  for(let i in info, tioPatinhas){
+	  console.log(info[i]+ " e " + tioPatinhas[i])
+  }
