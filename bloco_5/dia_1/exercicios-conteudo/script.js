70efccf8236d727f0ebb9072cb5ff5b99da021dc
Pedro Sehn <pedrorsehn@hotmail.com>
2021-05-26 19:15:09 -0300

    add exercicios do bloco 5 dia 1

---
+++ bloco_5/dia_1/exercicios-conteudo/script.js
@@ -0,0 +1,7 @@
+document.querySelector('#header-container').style.backgroundColor = 'green';
+document.getElementsByTagName('section')[0].style.backgroundColor = 'pink';
+document.getElementsByTagName('section')[1].style.backgroundColor = 'yellow';
+document.querySelectorAll('.urgent h3')[0].style.backgroundColor = 'purple';
+document.querySelectorAll('.urgent h3')[1].style.backgroundColor = 'purple';
+document.querySelectorAll('.non-urgent h3')[0].style.backgroundColor = 'black';
+document.querySelectorAll('.non-urgent h3')[1].style.backgroundColor = 'black';
